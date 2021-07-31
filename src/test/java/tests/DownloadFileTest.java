b8ad57237e78dbef1df60e79927bf56b4e091f71
Simon Postnikov <postnikov.sn@mail.ru>
2021-07-31 14:16:40 +0300

    added download file test

---
+++ src/test/java/tests/DownloadFileTest.java
@@ -0,0 +1,32 @@
+package tests;
+import com.codeborne.selenide.Configuration;
+import org.apache.commons.io.FileUtils;
+import org.apache.hc.core5.util.Asserts;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DownloadFileTest {
+    @Test
+    void selenideDownloadReadmeTest() throws IOException {
+        Configuration.downloadsFolder = "downloads";
+        open("https://github.com/selenide/selenide/blob/master/README.md");
+        File downloaderFile = $("#raw-url").download();
+        String fileContent = FileUtils.readFileToString(downloaderFile, StandardCharsets.UTF_8);
+       // assertTrue(fileContent.contains(" # Selenide = UI Testing Framework powered by Selenium WebDriver"));
+        System.out.println(fileContent);
+        assertThat(fileContent, containsString("Selenide = UI Testing Framework powered by Selenium WebDriver"));
+
+     //   # Selenide = UI Testing Framework powered by Selenium WebDriver
+
+    }
+}
